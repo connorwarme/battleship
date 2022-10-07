a7a1525eed44079177e19588ba9615b2260f8616
connorwarme <connor.warme@gmail.com>
2022-10-07 17:01:25 -0600

    Build initial shipFactory w/ tests

--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ console.log(`hello, world!`);
 const check = (i, e) => {
     return i+e;
 }
-export { check };
+const shipFactory = (length) => {
+    let hitCounter = 0;
+    let sunk = false;
+    const hit = () => {
+        return hitCounter += 1;
+    }
+    const isSunk = () => {
+        return length == hitCounter ? sunk = true : sunk = false;
+        }
+    return { length, hitCounter, sunk, hit, isSunk}
+    };
+const ship = shipFactory(5);
+ship.hit();
+console.log(ship.hitCounter);
+export { check, ship };
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,17 @@
 import {
-    check
+    check,
+    ship
 } from './index';
 
 test('if check provides sum of two arguments', () => {
     expect(check(1,3)).toBe(4);
 });
+test('shipFactory provides length correctly', () => {
+  expect(ship.length).toBe(5);
+});
+test('ship hit function adds to hitCounter', () => {
+  expect(ship.hitCounter).toBe(1);
+}); 
+test('does isSunk properly assess ship.sunk?', () => {
+  expect(ship.isSunk()).toBe(false);
+})
