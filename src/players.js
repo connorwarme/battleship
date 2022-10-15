e875c588669680b98503c385a2349f9e131dd20d
connorwarme <connor.warme@gmail.com>
2022-10-15 11:21:12 -0600

    Create new files, working to modularize

---
+++ src/players.js
@@ -0,0 +1,1 @@
+// player factory and AI logic
---
+++ src/ship.js
@@ -0,0 +1,24 @@
+// ship logic
+
+const ShipFactory = (input) => {
+    const length = input;
+    let hits = 0;
+    let sunk = false;
+    const getHits = () => {
+        return hits;
+    }
+    const hit = () => {
+        hits += 1;
+    }
+    const isSunk = () => {
+        return length <= hits ? sunk = true : sunk = false;
+    }
+    // added this - can I set hit and isSunk to private, and only export this one? !!!
+    const hitPlus = () => {
+        hit();
+        return isSunk();
+    }
+    return { length, sunk, getHits, hit, isSunk, hitPlus}
+};
+
+export default ShipFactory;
