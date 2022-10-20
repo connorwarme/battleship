c797656cd9f00c04204714e82a0bb2e6241b812c
connorwarme <connor.warme@gmail.com>
2022-10-20 12:29:49 -0600

    Place ship hover - hardcoded length and axis, but working

--- src/placeFleetDOM.js
+++ src/placeFleetDOM.js
@@ -1,13 +1,29 @@
 import { createElement } from "./utility";
 
+
+const highlightLength = (element, length) => {
+    const coord = Array.from(element.id);
+    for (let i = 1; i < length; i++) {
+        const newId = `${Number(coord[0])+i}${coord[1]}`;
+        const cell = document.getElementById(newId);
+        cell.style.backgroundColor = "lightgray";
+    }
+
+
+}
 const hover = (element) => {
     // on enter, highlight
     element.addEventListener('mouseenter', () => {
         // could add a class...
         element.style.backgroundColor = "lightgray";
+        highlightLength(element, 5);
     })
     element.addEventListener('mouseout', () => {
         element.style.backgroundColor = "white";
+        const grid = Array.from(document.querySelectorAll('div.cell'));
+        grid.forEach(cell => {
+            cell.style.backgroundColor = "white";
+        })
     })
 }
 
