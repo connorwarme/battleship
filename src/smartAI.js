6fcac8603c324fffe38f62d72c678172f703df9b
connorwarme <connor.warme@gmail.com>
2022-10-28 14:51:45 -0600

    Create target attackStack to draw from after AI lands a hit

--- src/smartAI.js
+++ src/smartAI.js
@@ -2,39 +2,62 @@ import BoardFactory from "./board";
 import { computer } from "./players";
 // implement improved tactics for AI player
 
-// target mode: after hit, return adjacent squares and attack those
-// target improved: if multiple hits are horizontal, prefer continued attacks along horizontal until sunk
-const neighborCells = (x, y) => {
-    const a = [x-1, y];
-    const b = [x+1, y];
-    const c = [x, y-1];
-    const d = [x, y+1];
-    return [a, b, c, d];
-}
-const checkCoords = (input) => {
-    if (input[0] >= 0 && input[0] <= 9 && input[1] >= 0 && input[1] <= 9) {
-        return input;
+const target = (() => {
+    // target mode: after hit, return adjacent squares and attack those
+    // target improved: if multiple hits are horizontal, prefer continued attacks along horizontal until sunk
+    const neighborCells = (x, y) => {
+        const a = [x-1, y];
+        const b = [x+1, y];
+        const c = [x, y-1];
+        const d = [x, y+1];
+        return [a, b, c, d];
     }
-    return false;
-}
-const checkCoordsArray = (input) => {
-    const returnValue = [];
-    for (let i = 0; i<input.length; i++) {
-        let value = checkCoords(input[i]);
-        if (value) {
-            returnValue.push(value);
+    const checkCoords = (input) => {
+        if (input[0] >= 0 && input[0] <= 9 && input[1] >= 0 && input[1] <= 9) {
+            return input;
         }
+        // also needs to check if the coords are an already selected cell...
+        return false;
     }
-    return returnValue;
-}
-const target = (x, y) => {
-    
-}
+    const checkCoordsArray = (input) => {
+        const returnValue = [];
+        for (let i = 0; i<input.length; i++) {
+            let value = checkCoords(input[i]);
+            if (value) {
+                returnValue.push(value);
+            }
+        }
+        return returnValue;
+    }
+    // if random attack lands a hit, run target instead of another random attack
+    // might need an array/stack that exists within AI player (outside of target);
+    // e.g. if (stack.length > 0) { ...run attack from stack coordinates... }
+    const attackStack = [];
+    const getStack = () => { 
+        return attackStack; 
+    };
+    const launchAttack = () => {
+        return attackStack.shift();
+    }
+    const afterHit = (hitCoord) => {
+        const surroundCells = neighborCells(hitCoord[0], hitCoord[1]);
+        const neighbors = checkCoordsArray(surroundCells);
+        neighbors.forEach(cell => {
+            attackStack.push(cell);
+        })
+        console.log(attackStack);
+    }
+    return { neighborCells, checkCoordsArray, getStack, launchAttack, afterHit };
+})();
 
+target.main([0, 0]);
+console.log(target.getStack());
+console.log(target.launchAttack());
+console.log(target.getStack());
 // hunt mode (random) improved: only attack every other square (while patrol boat exists)
 // -> then every 3rd, etc.
 // utilizing math concept of parity, based on (shortest) ship's length 
 
 // probability / heat map
 
-export { neighborCells, target, checkCoordsArray };
+export { target };
--- src/smartAI.test.js
+++ src/smartAI.test.js
@@ -1,18 +1,18 @@
 import BoardFactory from "./board";
 import { computer } from "./players";
-import { neighborCells, target, checkCoordsArray } from "./smartAI";
+import { target } from "./smartAI";
 
-const value = neighborCells(2, 2);
-const edge = neighborCells(0, 0);
-console.log(checkCoordsArray(edge));
+const value = target.neighborCells(2, 2);
+const edge = target.neighborCells(0, 0);
+console.log(target.checkCoordsArray(edge));
 test('Simple target will return coordinates neighboring hit', () => {
-    expect(neighborCells([2,2]).length).toBe(4);
+    expect(target.neighborCells([2,2]).length).toBe(4);
     expect(value[0]).toStrictEqual([1, 2]);
     expect(value[1]).toStrictEqual([3, 2]);
     expect(value[2]).toStrictEqual([2, 1]);
     expect(value[3]).toStrictEqual([2, 3]);
 })
 test('Check if neighboring cells are on gameboard', () => {
-    expect(checkCoordsArray(value).length).toBe(4);
-    expect(checkCoordsArray(edge).length).toBe(2)
+    expect(target.checkCoordsArray(value).length).toBe(4);
+    expect(target.checkCoordsArray(edge).length).toBe(2)
 })
