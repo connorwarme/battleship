c8ed2b5a28d49856be2e3956cf3f4b0c8a3c9c6b
connorwarme <connor.warme@gmail.com>
2022-11-22 11:03:46 -0700

    Clean up and prettier board.js

--- src/board.js
+++ src/board.js
@@ -1,176 +1,181 @@
 // board logic
 
 const BoardFactory = () => {
-    const board = [];
-    const create = (length) => {
-        for (let i = 0; i<length; i++) {
-            let row = [];
-            board.push(row);
-            for (let j = 0; j<length; j++) {
-                row.push(0);
-            }
+  const board = [];
+  const create = (length) => {
+    for (let i = 0; i < length; i++) {
+      let row = [];
+      board.push(row);
+      for (let j = 0; j < length; j++) {
+        row.push(0);
+      }
+    }
+    return board;
+  };
+  const shipArray = [];
+  const placeShip = (object, axis, x, y) => {
+    if (
+      checkOnBoard(object.length, axis, x, y) ||
+      checkPlace(object.length, axis, x, y)
+    ) {
+      return false;
+    }
+    if (axis) {
+      for (let i = 0; i < object.length; i++) {
+        board[x + i][y] = object.marker;
+        object.coords.push([x + i, y]);
+      }
+    } else {
+      for (let i = 0; i < object.length; i++) {
+        board[x][y + i] = object.marker;
+        object.coords.push([x, y + i]);
+      }
+    }
+    shipArray.push(object);
+    return true;
+  };
+  const checkPlace = (length, axis, x, y) => {
+    let array = [];
+    let value;
+    if (axis) {
+      for (let i = 0; i < length; i++) {
+        array.push(board[x + i][y] > 0);
+      }
+    } else {
+      for (let i = 0; i < length; i++) {
+        array.push(board[x][y + i] > 0);
+      }
+    }
+    value = array.find((index) => {
+      return index == true;
+    });
+    return value;
+  };
+  const checkMiss = (length, axis, x, y) => {
+    let array = [];
+    let value;
+    if (axis) {
+      for (let i = 0; i < length; i++) {
+        array.push(board[x + i][y] == -1);
+      }
+    } else {
+      for (let i = 0; i < length; i++) {
+        array.push(board[x][y + i] == -1);
+      }
+    }
+    value = array.find((index) => {
+      return index == true;
+    });
+    return value;
+  };
+  const checkProb = (length, axis, x, y, hitCoordArray) => {
+    let weight = 1;
+    const shipCoords = [];
+    for (let i = 0; i < length; i++) {
+      if (axis) {
+        shipCoords.push([Number(x) + i, y]);
+      } else {
+        shipCoords.push([x, Number(y) + i]);
+      }
+    }
+    hitCoordArray.forEach((coord) => {
+      // need to compare shipCoordinates with hitCoordinatesArray
+      // if yes, increase weight x 50
+      // if no, normal weight for cell counter for probMap
+      for (let i = 0; i < shipCoords.length; i++) {
+        if (coord[0] == shipCoords[i][0]) {
+          if (coord[1] == shipCoords[i][1]) {
+            weight *= 50;
+          }
         }
-        return board;
+      }
+    });
+    return weight;
+  };
+  const checkOnBoard = (length, axis, x, y) => {
+    let value;
+    if (axis) {
+      value = x + length;
+      return value > board.length;
+    } else {
+      value = y + length;
+      return value > board[x].length;
     }
-    const shipArray = [];
-    const placeShip = (object, axis, x, y) => {
-        if (checkOnBoard(object.length, axis, x, y) || 
-        checkPlace(object.length, axis, x, y)) {
-            return false;
+  };
+  const whichShip = (input) => {
+    return shipArray.find((index) => index.marker === input);
+  };
+  const allSunk = () => shipArray.every((index) => index.isSunk() === true);
+  const missedAttacks = [];
+  const _addMiss = (x, y) => missedAttacks.push([x, y]);
+  const _updateBoard = (x, y, value) => (board[x][y] = value);
+  const _updateMiss = (x, y) => {
+    _addMiss(x, y);
+    _updateBoard(x, y, -1);
+  };
+  const _updateHit = (x, y) => {
+    const theShip = whichShip(board[x][y]);
+    const sunk = theShip.hitPlus();
+    _updateBoard(x, y, board[x][y] + 10);
+    return sunk;
+  };
+  const _checkAttack = (x, y) => {
+    if (board[x][y] > 10 || board[x][y] == -1) {
+      return false;
+    }
+    return true;
+  };
+  const _launchAttack = (x, y, obj) => {
+    obj.value = board[x][y];
+    if (obj.value > 0) {
+      // if a hit:
+      // -> check which ship it is, add hit (to board & to display)
+      // -> see if it sunk and update accordingly
+      // -> change return from value to true
+      const sunk = _updateHit(x, y);
+      if (sunk) {
+        obj.sunk = true;
+        if (allSunk()) {
+          obj.allSunk = true;
+          console.log("Game over");
         }
-        if (axis) {
-            for (let i = 0; i<object.length; i++) {
-                board[x+i][y] = object.marker;
-                object.coords.push([x+i, y]);
-        }} else {
-            for (let i = 0; i<object.length; i++) {
-                board[x][y+i] = object.marker;
-                object.coords.push([x, y+i]);
-            }
-        }
-        shipArray.push(object);
-        return true;
+      }
+      obj.ship = obj.value;
+      obj.value = true;
+    } else {
+      // aka a miss:
+      // -> update gameboard w/ -1 (for miss)
+      // -> update display
+      _updateMiss(x, y);
+      obj.value = false;
     }
-    const checkPlace = (length, axis, x, y) => {
-        let array = [];
-        let value;
-        if (axis) {
-            for (let i = 0; i<length; i++) {
-                array.push(board[x+i][y] > 0);
-            }
-        } else {
-            for (let i = 0; i<length; i++) {
-                array.push(board[x][y+i] > 0);
-            }
-        }
-        value = array.find(index => {
-            return index == true;
-        });
-        return value;
+    return obj;
+  };
+  const receiveAttack = (x, y) => {
+    const obj = {
+      sunk: false,
+      allSunk: false,
+    };
+    if (_checkAttack(x, y)) {
+      return _launchAttack(x, y, obj);
     }
-    const checkMiss = (length, axis, x, y) => {
-        let array = [];
-        let value;
-        if (axis) {
-            for (let i = 0; i<length; i++) {
-                array.push(board[x+i][y] == -1);
-            }
-        } else {
-            for (let i = 0; i<length; i++) {
-                array.push(board[x][y+i] == -1);
-            }
-        }
-        value = array.find(index => {
-            return index == true;
-        });
-        return value;
-    }
-    const checkProb = (length, axis, x, y, hitCoordArray) => {
-        let weight = 1;
-        const shipCoords = [];
-        for (let i=0; i<length; i++) {
-            if (axis) {
-                shipCoords.push([Number(x)+i, y]);
-            } else {
-                shipCoords.push([x, Number(y)+i]);
-            }
-        }
-        hitCoordArray.forEach(coord => {
-            for (let i=0; i<shipCoords.length; i++) {
-                if (coord[0] == shipCoords[i][0]) {
-                    if (coord[1] == shipCoords[i][1]) {
-                        weight *= 50;
-                    }
-                }
-            }
-        })
-        return weight;
-        // need to compare shipCoordinates with hitCoordinatesArray (needs to be a fn parameter)
-        // if yes, increase weight by factor of 5
-        // if no, normal weight for cell counter for probMap
-    }
-    const checkOnBoard = (length, axis, x, y) => {
-        let value;
-        if (axis) {
-            value = x + length;
-            return value > board.length;
-        } else {
-            value = y + length;
-            return value > board[x].length;
-        }
-    }
-    const whichShip = (input) => {
-        return shipArray.find(index => index.marker === input);
-    }
-    // pretty sure allSunk is working, 
-    // but I didn't manipulate the board to have all the ships sunk to test it fyi !!!
-    const allSunk = () => shipArray.every(index => index.isSunk() === true);
-    const missedAttacks = [];
-    const _addMiss = (x, y) => missedAttacks.push([x, y]);
-    const _updateBoard = (x, y, value) => board[x][y] = value;
-    const _updateMiss = (x, y) => {
-        _addMiss(x, y);
-        _updateBoard(x, y, -1);
-    }
-    const _updateHit = (x, y) => {
-        const theShip = whichShip(board[x][y]);
-        const sunk = theShip.hitPlus();
-        _updateBoard(x, y, (board[x][y] + 10));
-        return sunk;
-    }
-    const _checkAttack = (x, y) => {
-        if (board[x][y] > 10 || board[x][y] == -1) {
-            return false;
-        }
-        return true;
-    }
-    const _launchAttack = (x, y, obj) => {
-        obj.value = board[x][y];
-        if (obj.value > 0) {
-            const sunk = _updateHit(x, y);
-            if (sunk) {
-                obj.sunk = true;
-                if (allSunk()) {
-                    obj.allSunk = true;
-                    console.log('Game over');
-                    // need to fire gameOver function
-                    // what all is needed inside?
-                };
-            }
-            obj.ship = obj.value;
-            // check which ship it is
-            // run hit() on that ship
-            // see if it sunk and update accordingly - send to DOM fn to update display?
-            // thinking that you call a DOM fn with "sunk" boolean.. !!!
-            // update board
-            // send signal to DOM to update grid with hit mark !!!
-            // needs to switch turns !!!
-            // change return from value to true
-            obj.value = true
-        } else {
-            // add miss to array
-            // update gameboard w/ -1 (for miss)
-            _updateMiss(x, y);
-            // obj.value might need to be "-1" !!!
-            obj.value = false;
-            // send signal to DOM function to update grid with miss mark !!!
-            // needs to switch turns !!!
-        }
-        return obj;
-    }
-    const receiveAttack = (x, y) => {
-        const obj = {
-            sunk: false,
-            allSunk: false,
-        };
-        if (_checkAttack(x, y)) {
-            return _launchAttack(x, y, obj);
-        } 
-        // need to loop back -> need user to give new coordinates
-        obj.value = 0;
-        return obj;
-    }   
-    return { board, create, placeShip, checkPlace, checkMiss, checkProb, checkOnBoard, whichShip, receiveAttack, allSunk };
+    // else, user selected ineligible coordinates
+    // and new coordinates needed
+    obj.value = 0;
+    return obj;
+  };
+  return {
+    board,
+    create,
+    placeShip,
+    checkPlace,
+    checkMiss,
+    checkProb,
+    checkOnBoard,
+    whichShip,
+    receiveAttack,
+    allSunk,
+  };
 };
 
 export default BoardFactory;
