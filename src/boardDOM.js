6e2eea40666d47fc723e6d55b6fc4b7677d4ca4c
connorwarme <connor.warme@gmail.com>
2022-11-22 11:09:53 -0700

    Clean up and prettier boardDOM.js

--- src/boardDOM.js
+++ src/boardDOM.js
@@ -2,93 +2,93 @@ import { createElement } from "./utility";
 import loop from "./gameloop";
 
 const attackHover = (element) => {
-        // on enter, highlight
-        element.addEventListener('mouseenter', () => {
-            // check if cell has been clicked already
-            if (element.classList.value == 'cell') {
-                element.style.cursor = "crosshair";
-                element.style.backgroundColor = "#38B000";
-            } else {
-                element.style.cursor = "not-allowed";
-                element.style.backgroundColor = "#000000";
-            }
-        })
-        element.addEventListener('mouseout', () => {
-            const grid = Array.from(element.parentElement.children);
-            // const grid = Array.from(document.querySelectorAll('div.cell'));
-            grid.forEach(cell => {
-                if (cell.classList.length == 1) {
-                    cell.style.backgroundColor = "white";
-                } else if (cell.classList.value == "cell hit") {
-                    cell.style.backgroundColor = "rgb(208, 0, 0)";
-                } else if (cell.classList.value == "cell miss") {
-                    cell.style.backgroundColor = "rgb(35, 100, 170)"
-                } else if (cell.classList.value == "cell sunk") {
-                    cell.style.backgroundColor = "rgb(208, 0, 0)";
-                }
-            })
-        })
+  // on enter, highlight
+  element.addEventListener("mouseenter", () => {
+    // check if cell has been clicked already
+    if (element.classList.value == "cell") {
+      element.style.cursor = "crosshair";
+      element.style.backgroundColor = "#38B000";
+    } else {
+      element.style.cursor = "not-allowed";
+      element.style.backgroundColor = "#000000";
     }
+  });
+  element.addEventListener("mouseout", () => {
+    const grid = Array.from(element.parentElement.children);
+    grid.forEach((cell) => {
+      if (cell.classList.length == 1) {
+        cell.style.backgroundColor = "white";
+      } else if (cell.classList.value == "cell hit") {
+        cell.style.backgroundColor = "rgb(208, 0, 0)";
+      } else if (cell.classList.value == "cell miss") {
+        cell.style.backgroundColor = "rgb(35, 100, 170)";
+      } else if (cell.classList.value == "cell sunk") {
+        cell.style.backgroundColor = "rgb(208, 0, 0)";
+      }
+    });
+  });
+};
 // render gameboards on page
 const buildBoard = (object, boolean, funct) => {
-    const board = object.board;
-    const displayBoard = createElement('div', {class: 'displayBoard'});
-    for (let i = 0; i<board.length; i++) {
-        const gridCol = createElement('div', {class: `col`, id: `${i}`});
-        displayBoard.appendChild(gridCol);
-        for (let j = 0; j < board[i].length; j++) {
-            const gridCell = createElement('div', {class: `cell`, id: `${i}${j}`});
-            if (boolean) {
-                // need to be able to delineate event listener for place ships and other for actual gameplay
-                gridCell.addEventListener('click', (e) => {
-                    if (loop.getTurn()) {
-                        funct([i, j], e);
-                    }
-                })
-                attackHover(gridCell);
-            }
-            gridCol.appendChild(gridCell);
-        }
+  const board = object.board;
+  const displayBoard = createElement("div", { class: "displayBoard" });
+  for (let i = 0; i < board.length; i++) {
+    const gridCol = createElement("div", { class: `col`, id: `${i}` });
+    displayBoard.appendChild(gridCol);
+    for (let j = 0; j < board[i].length; j++) {
+      const gridCell = createElement("div", { class: `cell`, id: `${i}${j}` });
+      if (boolean) {
+        // add click listener for gameplay
+        // and add attack hover feature
+        gridCell.addEventListener("click", (e) => {
+          if (loop.getTurn()) {
+            funct([i, j], e);
+          }
+        });
+        attackHover(gridCell);
+      }
+      gridCol.appendChild(gridCell);
     }
-    return displayBoard;
-}
+  }
+  return displayBoard;
+};
 const displayShips = (object, boardDOM) => {
-    const board = object.board;
-    for (let i = 0; i<board.length; i++) {
-        for (let j = 0; j<board[i].length; j++) {
-            if (board[i][j] > 0) {
-                boardDOM.children[i].children[j].classList.add('ship');
-            }
-        }
+  const board = object.board;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] > 0) {
+        boardDOM.children[i].children[j].classList.add("ship");
+      }
     }
-}
+  }
+};
 const markCell = (x, y, result, gb, boardDOM, emoji) => {
-    if (result.sunk) {
-        const ship = gb.whichShip(result.ship);
-        ship.coords.forEach(coord => {
-            boardDOM.children[coord[0]].children[coord[1]].classList = 'cell sunk';
-            boardDOM.children[coord[0]].children[coord[1]].textContent = `${emoji}`;
-        })
-    } else if (result.value) {
-        boardDOM.children[x].children[y].classList.add('hit');
-        boardDOM.children[x].children[y].textContent = "🔥";
-    } else {
-        boardDOM.children[x].children[y].classList.add('miss');
-        boardDOM.children[x].children[y].textContent = "🌊";
-    }
-}
+  if (result.sunk) {
+    const ship = gb.whichShip(result.ship);
+    ship.coords.forEach((coord) => {
+      boardDOM.children[coord[0]].children[coord[1]].classList = "cell sunk";
+      boardDOM.children[coord[0]].children[coord[1]].textContent = `${emoji}`;
+    });
+  } else if (result.value) {
+    boardDOM.children[x].children[y].classList.add("hit");
+    boardDOM.children[x].children[y].textContent = "🔥";
+  } else {
+    boardDOM.children[x].children[y].classList.add("miss");
+    boardDOM.children[x].children[y].textContent = "🌊";
+  }
+};
 const displayLabel = (message) => {
-    const labelContainer = createElement('div', {class: "labelContainer"});
-    const label = createElement('div', {class: 'boardLabel'});
-    label.textContent = `${message} waters`;
-    labelContainer.appendChild(label);
-    return labelContainer;
-}
+  const labelContainer = createElement("div", { class: "labelContainer" });
+  const label = createElement("div", { class: "boardLabel" });
+  label.textContent = `${message} waters`;
+  labelContainer.appendChild(label);
+  return labelContainer;
+};
 const buildBoardAndLabel = (object, boolean, message, funct) => {
-    const boardContainer = createElement('div', {class: "boardContainer"});
-    boardContainer.appendChild(displayLabel(message));
-    boardContainer.appendChild(buildBoard(object, boolean, funct));
-    return boardContainer;
-}
+  const boardContainer = createElement("div", { class: "boardContainer" });
+  boardContainer.appendChild(displayLabel(message));
+  boardContainer.appendChild(buildBoard(object, boolean, funct));
+  return boardContainer;
+};
 
 export { buildBoard, displayShips, markCell, buildBoardAndLabel };
