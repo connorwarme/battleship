135e09b57aa5d6b4bc184209fd1aa15b64e0ff73
connorwarme <connor.warme@gmail.com>
2022-11-25 19:42:00 -0700

    Prettier utility.js

--- src/utility.js
+++ src/utility.js
@@ -2,19 +2,19 @@
 // -> create DOM element and add attributes
 // is there a more effective/efficient way to do this?!
 const createElement = (type, attributes) => {
-    const newElement = document.createElement(`${type}`);
-    if (attributes == null) return newElement;
-    setAttributes(newElement, attributes);
-    return newElement;
-}
+  const newElement = document.createElement(`${type}`);
+  if (attributes == null) return newElement;
+  setAttributes(newElement, attributes);
+  return newElement;
+};
 const setAttributes = (element, attributes) => {
-    Object.entries(attributes).forEach(([key, value]) => {
-        element.setAttribute(key, value);
-    })
-}
+  Object.entries(attributes).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+};
 const clearDiv = (div) => {
-    while (div.children.length > 0) {
-        div.removeChild(div.firstChild);
-    }
-}
+  while (div.children.length > 0) {
+    div.removeChild(div.firstChild);
+  }
+};
 export { createElement, clearDiv };
