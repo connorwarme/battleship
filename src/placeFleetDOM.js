67b865268b14d189e41961cc2e9ac7bfcfa1a53c
connorwarme <connor.warme@gmail.com>
2022-11-25 19:19:30 -0700

    Clean up, comment, and prettier placeFleetDOM

--- src/placeFleetDOM.js
+++ src/placeFleetDOM.js
@@ -1,167 +1,168 @@
 import { createElement } from "./utility";
 import { buildBoard, displayShips } from "./boardDOM";
-import { com } from './comsDOM';
-import loop from './gameloop';
+import { com } from "./comsDOM";
+import loop from "./gameloop";
 
 const place = (() => {
-    let axis = true;
-    const currentAxis = () => {
-        return axis;
+  let axis = true;
+  const currentAxis = () => {
+    return axis;
+  };
+  const createDisplay = (opponent) => {
+    const container = createElement("div", { class: "placeContainer" });
+    const buttonC = createElement("div", { class: "placeBtnContainer" });
+    const axisBtn = createElement("button", { class: "axisBtn" });
+    axisBtn.textContent = `Rotate`;
+    const randomBtn = createElement("button", { class: "randomBtn" });
+    randomBtn.textContent = "Random";
+    const instruct = createElement("div", { class: "placeInstructions" });
+    const instructTitle = createElement("div", { class: "instructTitle" });
+    instructTitle.textContent = "Instructions:";
+    const instructText1 = createElement("div", { class: "instructText" });
+    instructText1.textContent = `Select coordinates on board to place current ship.`;
+    const instructText2 = createElement("div", { class: "instructText" });
+    instructText2.textContent = `Use buttons to rotate ship or randomly deploy entire fleet.`;
+    // add listeners
+    axisBtn.addEventListener("click", () => {
+      axis = !axis;
+      // delete current board
+      currentDOM.removeChild(currentDOM.lastChild);
+      // build new board w/ correct listeners
+      perShip(currentDOM, currentBoard, currentShip);
+    });
+    randomBtn.addEventListener("click", () => {
+      // place user fleet, finish placeFleet & instantiate next step
+      opponent.player.placeFleet(currentObj);
+      currentDOM.removeChild(currentDOM.lastChild);
+      finish();
+    });
+    // append it all together
+    container.appendChild(buttonC);
+    buttonC.appendChild(axisBtn);
+    buttonC.appendChild(randomBtn);
+    container.appendChild(instruct);
+    instruct.appendChild(instructTitle);
+    instruct.appendChild(instructText1);
+    instruct.appendChild(instructText2);
+    return container;
+  };
+  // check if coordinates yield ship placement on the board
+  // or if they conflict with already placed ship
+  const checkCell = (gameboard, ship, axis, x, y) => {
+    if (
+      gameboard.checkOnBoard(ship.length, axis, x, y) ||
+      gameboard.checkPlace(ship.length, axis, x, y)
+    ) {
+      return false;
     }
-    const createDisplay = (opponent) => {
-        const container = createElement('div', {class: "placeContainer"});
-        const buttonC = createElement('div', {class: 'placeBtnContainer'});
-        const axisBtn = createElement('button', {class: 'axisBtn'});
-        axisBtn.textContent = `Rotate`
-        const randomBtn = createElement('button', {class: 'randomBtn'});
-        randomBtn.textContent = 'Random';
-        const instruct = createElement('div', {class: 'placeInstructions'});
-        const instructTitle = createElement('div', {class: 'instructTitle'});
-        instructTitle.textContent = "Instructions:"
-        const instructText1 = createElement('div', {class: "instructText"});
-        instructText1.textContent = `Select coordinates on board to place current ship.`;
-        const instructText2 = createElement('div', {class: "instructText"});
-        instructText2.textContent = `Use buttons to rotate ship or randomly deploy entire fleet.`
-
-        // add listeners
-        axisBtn.addEventListener('click', () => {
-            // axis = !axis;
-            // let text = axis ? 'X' : 'Y';
-            // axisBtn.textContent = `Axis: ${text}`;
-            axisBtn.textContent = 'Rotate';
-            // delete current board
-            currentDOM.removeChild(currentDOM.lastChild);
-            // build new board w/ correct listeners
-            perShip(currentDOM, currentBoard, currentShip);
-        })
-        randomBtn.addEventListener('click', () => {
-            opponent.player.placeFleet(currentObj);
-            currentDOM.removeChild(currentDOM.lastChild);
-            finish();
-        })
-
-        // append it all together
-        container.appendChild(buttonC);
-        buttonC.appendChild(axisBtn);
-        buttonC.appendChild(randomBtn);
-        container.appendChild(instruct);
-        instruct.appendChild(instructTitle);
-        instruct.appendChild(instructText1);
-        instruct.appendChild(instructText2);
-
-        return container;
+    return true;
+  };
+  const highlightLength = (element, length) => {
+    const coord = Array.from(element.id);
+    let newId;
+    for (let i = 0; i < length; i++) {
+      if (axis) {
+        newId = `${Number(coord[0]) + i}${coord[1]}`;
+      } else {
+        newId = `${coord[0]}${Number(coord[1]) + i}`;
+      }
+      const cell = document.getElementById(newId);
+      cell.style.backgroundColor = "slategray";
     }
-    // 
-    const checkCell = (gameboard, ship, axis, x, y) => {
-        if (gameboard.checkOnBoard(ship.length, axis, x, y) || 
-        gameboard.checkPlace(ship.length, axis, x, y)) {
-            return false;
+  };
+  const clickFn = (x, y) => {
+    currentObj.gb.placeShip(currentShip, axis, x, y);
+    count++;
+    currentDOM.removeChild(currentDOM.lastChild);
+    nextShip(currentDOM, currentObj);
+  };
+  const clickAttack = (element, gameboard, ship, x, y) => {
+    if (checkCell(gameboard, ship, currentAxis(), x, y)) {
+      element.addEventListener("click", () => {
+        clickFn(x, y);
+      });
+    }
+  };
+  const hover = (element, gameboard, ship, x, y) => {
+    // on enter, highlight (gray is valid, red is not-allowed)
+    element.addEventListener("mouseenter", () => {
+      // could add a class...
+      if (checkCell(gameboard, ship, currentAxis(), x, y)) {
+        element.style.cursor = "pointer";
+        highlightLength(element, ship.length);
+      } else {
+        element.style.cursor = "not-allowed";
+        element.style.backgroundColor = "rgb(208, 0, 0)";
+      }
+    });
+    // on exit, remove hover background color
+    element.addEventListener("mouseout", () => {
+      const grid = Array.from(document.querySelectorAll("div.cell"));
+      grid.forEach((cell) => {
+        if (cell.classList.length == 1) {
+          cell.style.backgroundColor = "white";
+        } else if (cell.classList.length == 2) {
+          cell.style.backgroundColor = "slategray";
         }
-        return true;
+      });
+    });
+  };
+  const addCellListeners = (gameboard, ship) => {
+    const cellArray = Array.from(document.querySelectorAll("div.cell"));
+    cellArray.forEach((cell) => {
+      const x = Number(cell.id.charAt(0));
+      const y = Number(cell.id.charAt(1));
+      hover(cell, gameboard, ship, x, y);
+      clickAttack(cell, gameboard, ship, x, y);
+    });
+  };
+  let currentObj;
+  let currentBoard;
+  let currentShip;
+  let fleetKeys;
+  let currentDOM;
+  let count = 0;
+  const updateAllCurrents = (parent, object) => {
+    currentDOM = parent;
+    currentObj = object;
+    currentBoard = object.gb;
+    fleetKeys = Object.keys(object.fleet);
+  };
+  const updateCurrentShip = (ship) => {
+    currentShip = ship;
+  };
+  const nextShip = (parentDOM, playerObj) => {
+    if (count < 5) {
+      com.instruct(playerObj.player, fleetKeys[count]);
+      perShip(parentDOM, playerObj.gb, playerObj.fleet[fleetKeys[count]]);
+    } else {
+      console.log("Next step of gameloop!");
+      finish();
     }
-    const highlightLength = (element, length) => {
-        const coord = Array.from(element.id);
-        let newId;
-        for (let i = 0; i < length; i++) {
-            if (axis) {
-                newId = `${Number(coord[0])+i}${coord[1]}`;
-            } else {
-                newId = `${coord[0]}${Number(coord[1])+i}`;
-            }
-            const cell = document.getElementById(newId);
-            cell.style.backgroundColor = "slategray";
-        }
+  };
+  const perShip = (parentDOM, playerBoard, ship) => {
+    updateCurrentShip(ship);
+    parentDOM.appendChild(buildBoard(playerBoard, false));
+    displayShips(currentBoard, currentDOM.children[0]);
+    addCellListeners(playerBoard, ship);
+  };
+  const start = (parentDOM, playerObj, opponentObj) => {
+    parentDOM.parentElement.parentElement.children[1].appendChild(
+      createDisplay(opponentObj)
+    );
+    updateAllCurrents(parentDOM, playerObj);
+    nextShip(parentDOM, playerObj);
+  };
+  const finish = () => {
+    com.clear();
+    // invoke next step in gameloop, battle mode
+    // how can I smooth this transition? (ease-in)? !!!
+    loop.launchGame(currentDOM);
+    if (window.matchMedia("(min-width: 550px)")) {
+      currentDOM.parentElement.parentElement.children[0].children[1].style.display =
+        "block";
     }
-    const clickFn = (x, y) => {
-        currentObj.gb.placeShip(currentShip, axis, x, y);
-        count++;
-        currentDOM.removeChild(currentDOM.lastChild);
-        nextShip(currentDOM, currentObj);
-    }
-    const clickAttack = (element, gameboard, ship, x, y) => {
-        if (checkCell(gameboard, ship, currentAxis(), x, y)) {
-            element.addEventListener('click', () => {
-                clickFn(x, y);
-            })
-        }
-    }
-    const hover = (element, gameboard, ship, x, y) => {
-        // on enter, highlight
-        element.addEventListener('mouseenter', () => {
-            // could add a class...
-            if (checkCell(gameboard, ship, currentAxis(), x, y)) {
-                element.style.cursor = "pointer";
-                highlightLength(element, ship.length);
-            } else {
-                element.style.cursor = "not-allowed";
-                element.style.backgroundColor = "rgb(208, 0, 0)";
-            }
-        })
-        element.addEventListener('mouseout', () => {
-            const grid = Array.from(document.querySelectorAll('div.cell'));
-            grid.forEach(cell => {
-                if (cell.classList.length == 1) {
-                    cell.style.backgroundColor = "white";
-                } else if (cell.classList.length == 2) {
-                    cell.style.backgroundColor = "slategray";
-                }
-            })
-        })
-    }
-    const addCellListeners = (gameboard, ship) => {
-        const cellArray = Array.from(document.querySelectorAll('div.cell'));
-        cellArray.forEach(cell => {
-            const x = Number(cell.id.charAt(0));
-            const y = Number(cell.id.charAt(1));
-            hover(cell, gameboard, ship, x, y);
-            clickAttack(cell, gameboard, ship, x, y);
-        })
-    }
-    let currentObj;
-    let currentBoard;
-    let currentShip;
-    let fleetKeys;
-    let currentDOM;
-    let count = 0;
-    const updateAllCurrents = (parent, object) => {
-        currentDOM = parent;
-        currentObj = object;
-        currentBoard = object.gb;
-        fleetKeys = fleetKeys = Object.keys(object.fleet);
-    }
-    const updateCurrentShip = (ship) => {
-        currentShip = ship;
-    }
-    const nextShip = (parentDOM, playerObj) => {
-        if (count < 5) {
-            com.instruct(playerObj.player, fleetKeys[count]);
-            perShip(parentDOM, playerObj.gb, playerObj.fleet[fleetKeys[count]]);
-        } else {
-            console.log('next step of gameloop!');
-            finish();
-        }
-    }
-    const perShip = (parentDOM, playerBoard, ship) => {
-        updateCurrentShip(ship);
-        parentDOM.appendChild(buildBoard(playerBoard, false));
-        displayShips(currentBoard, currentDOM.children[0]);
-        addCellListeners(playerBoard, ship);
-    }
-    const start = (parentDOM, playerObj, opponentObj) => {
-        parentDOM.parentElement.parentElement.children[1].appendChild(createDisplay(opponentObj));
-        updateAllCurrents(parentDOM, playerObj);
-        nextShip(parentDOM, playerObj);
-    }
-    const finish = () => {
-        com.clear();
-        // invoke gameloop step2 -> where 2 boards are displayed
-        // a way to transition this (ease-in)? !!!
-        loop.launchGame(currentDOM);
-        if (window.matchMedia('(min-width: 550px)')) {
-            currentDOM.parentElement.parentElement.children[0].children[1].style.display = "block";
-        }
-    }
-    return { hover, start };
+  };
+  return { hover, start };
 })();
-
 export { place };
