e02db355dcb0fa6c9c822561fc007e0aece747c0
connorwarme <connor.warme@gmail.com>
2022-11-25 19:34:05 -0700

    Clean and prettier ship.js

--- src/ship.js
+++ src/ship.js
@@ -1,37 +1,34 @@
 // ship logic
-
 const ShipFactory = (input, mark) => {
-    const length = input;
-    let hits = 0;
-    let sunk = false;
-    let marker = length;
-    if (mark) {
-        marker = mark;
-    }
-    const coords = [];
-    const getHits = () => {
-        return hits;
-    }
-    const hit = () => {
-        hits += 1;
-    }
-    const isSunk = () => {
-        return length <= hits ? sunk = true : sunk = false;
-    }
-    // added this - can I set hit and isSunk to private, and only export this one? !!!
-    const hitPlus = () => {
-        hit();
-        return isSunk();
-    }
-    return { length, sunk, marker, coords, getHits, hit, isSunk, hitPlus}
+  const length = input;
+  let hits = 0;
+  let sunk = false;
+  let marker = length;
+  if (mark) {
+    marker = mark;
+  }
+  const coords = [];
+  const getHits = () => {
+    return hits;
+  };
+  const hit = () => {
+    hits += 1;
+  };
+  const isSunk = () => {
+    return length <= hits ? (sunk = true) : (sunk = false);
+  };
+  const hitPlus = () => {
+    hit();
+    return isSunk();
+  };
+  return { length, sunk, marker, coords, getHits, hit, isSunk, hitPlus };
 };
 const buildFleet = () => {
-    const submarine = ShipFactory(3, 1);
-    const patrol = ShipFactory(2);
-    const destroyer = ShipFactory(3);
-    const battleship = ShipFactory(4);
-    const carrier = ShipFactory(5);
-    return { submarine, patrol, destroyer, battleship, carrier }
-}
-
+  const submarine = ShipFactory(3, 1);
+  const patrol = ShipFactory(2);
+  const destroyer = ShipFactory(3);
+  const battleship = ShipFactory(4);
+  const carrier = ShipFactory(5);
+  return { submarine, patrol, destroyer, battleship, carrier };
+};
 export { ShipFactory, buildFleet };
